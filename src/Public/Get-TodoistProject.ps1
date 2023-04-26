a0adc9c46f33201ee9fb24e0887a9f1b2a32eed3
Harry Joyce <65245441+harry-joyce@users.noreply.github.com>
2023-04-26 13:39:07 +0100

    Update Get-TodoistProject.ps1

--- src/Public/Get-TodoistProject.ps1
+++ src/Public/Get-TodoistProject.ps1
@@ -32,7 +32,7 @@ function Get-TodoistProject
 
     process
     {
-        $Request = Invoke-RestMethod -Headers $Header -Method Get -Uri 'https://api.todoist.com/rest/v1/projects' 
+        $Request = Invoke-RestMethod -Headers $Header -Method Get -Uri 'https://api.todoist.com/rest/v2/projects' 
         
         $projects = @()
         
