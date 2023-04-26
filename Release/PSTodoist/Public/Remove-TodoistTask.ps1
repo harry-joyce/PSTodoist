d1e7010a4f0595067f95873dc6dceefaee14ecc6
Harry Joyce <65245441+harry-joyce@users.noreply.github.com>
2023-04-26 13:35:36 +0100

    Update Remove-TodoistTask.ps1

--- Release/PSTodoist/Public/Remove-TodoistTask.ps1
+++ Release/PSTodoist/Public/Remove-TodoistTask.ps1
@@ -39,7 +39,7 @@ function Remove-TodoistTask
         
         $Id | Foreach {
         
-            $Uri = "https://api.todoist.com/rest/v1/tasks/$_"
+            $Uri = "https://api.todoist.com/rest/v2/tasks/$_"
             Write-Output $Uri
             $Request = Invoke-RestMethod -Headers $Header -Method Delete -Uri $Uri
             $Requests += $Request
